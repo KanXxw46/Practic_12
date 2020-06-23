34283ef2e09eb12fef7316bb1fb2d39c7d299f29
KanXxw46 <1kan_andreikkkk@mail.ru>
2020-06-23 11:32:53 +0600

    Practics

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practic_12
+{
+    class Counter
+    {
+        public int Value { get; set; }
+
+        public static bool operator true(Counter c1)
+        {
+            return c1.Value != 0;
+        }
+        public static bool operator false(Counter c1)
+        {
+            return c1.Value == 0;
+        }
+    }
+
+}
